50495f72f0e5d745b7961e95eb8e65bf9568cf8c
ziyangfan <1229417754@qq.com>
2024-10-19 15:07:12 +0800

    implement dcgan training.

---
+++ dcgan/train.py
@@ -0,0 +1,173 @@
+import os
+from argparse import Namespace, ArgumentParser
+import torch
+from torch import nn, Tensor
+from torchvision import datasets, transforms
+from torchvision.utils import save_image, make_grid
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import DataLoader
+from dcgan import Generator, Discriminator
+
+# Image processing.
+transform_mnist = transforms.Compose([
+    transforms.Resize(32),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=(0.5), std=(0.5))])
+
+transform_cifar = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
+
+# Device configuration.
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def denormalize(x: Tensor) -> Tensor:
+    out = (x + 1) / 2
+    return out.clamp(0, 1)
+
+
+def init_weight(module: nn.Module) -> None:
+    if isinstance(module, (nn.Linear, nn.Conv2d, nn.ConvTranspose2d)):
+        nn.init.normal_(module.weight, mean=0, std=0.02)
+        nn.init.constant_(module.bias, 0)
+
+
+def get_args() -> Namespace:
+    """Get commandline arguments."""
+    parser = ArgumentParser()
+    parser.add_argument('--lr', type=float, default=0.0002, help='learning rate for Adam optimizer')
+    parser.add_argument('--beta1', type=float, default=0.5, help='first momentum term for Adam')
+    parser.add_argument('--beta2', type=float, default=0.999, help='second momentum term for Adam')
+    parser.add_argument('--batch_size', type=int, default=128, help='size of a mini-batch')
+    parser.add_argument('--num_epochs', type=int, default=200, help='training epochs')
+    parser.add_argument('--latent_dim', type=int, default=100, help='dimensionality of the latent space')
+    parser.add_argument('--dataset', type=str, default='MNIST', help='training dataset(MNIST | FashionMNIST | CIFAR10)')
+    parser.add_argument('--sample_dir', type=str, default='samples', help='directory of image samples')
+    parser.add_argument('--interval', type=int, default=1, help='epoch interval between image samples')
+    parser.add_argument('--logdir', type=str, default='runs', help='directory of running log')
+    parser.add_argument('--ckpt_dir', type=str, default='checkpoints', help='directory for saving model checkpoints')
+    parser.add_argument('--seed', type=str, default=10213, help='random seed')
+    return parser.parse_args()
+
+
+def setup(args: Namespace) -> None:
+    torch.manual_seed(args.seed)
+    # Create directory if not exists.
+    if not os.path.exists(os.path.join(args.sample_dir, args.dataset)):
+        os.makedirs(os.path.join(args.sample_dir, args.dataset))
+    if not os.path.exists(os.path.join(args.ckpt_dir, args.dataset)):
+        os.makedirs(os.path.join(args.ckpt_dir, args.dataset))
+
+
+def get_data_loader(args: Namespace) -> DataLoader:
+    """Get data loader."""
+    if args.dataset == 'MNIST':
+        data = datasets.MNIST(root='../data', train=True, download=True, transform=transform_mnist)
+    elif args.dataset == 'FashionMNIST':
+        data = datasets.FashionMNIST(root='../data', train=True, download=True, transform=transform_mnist)
+    elif args.dataset == 'CIFAR10':
+        data = datasets.CIFAR10(root='../data', train=True, download=True, transform=transform_cifar)
+    else:
+        raise ValueError(f'Unkown dataset: {args.dataset}, support dataset: MNIST | FashionMNIST | CIFAR10')
+    return DataLoader(dataset=data, batch_size=args.batch_size, num_workers=4, shuffle=True)
+
+
+def train(args: Namespace, 
+          G: Generator, D: Discriminator, 
+          data_loader: DataLoader) -> None:
+    """Train Generator and Discriminator.
+
+    Args:
+        args(Namespace): arguments.
+        G(Generator): Generator in GAN.
+        D(Discriminator): Discriminator in GAN.
+    """
+    writer = SummaryWriter(os.path.join(args.logdir, args.dataset))
+
+    # generate fixed noise for sampling.
+    fixed_noise = torch.rand(64, args.latent_dim).to(device)
+
+    # Loss and optimizer.
+    criterion = nn.BCELoss().to(device)
+    optimizer_G = torch.optim.Adam(G.parameters(), lr=args.lr, betas=(args.beta1, args.beta2))
+    optimizer_D = torch.optim.Adam(D.parameters(), lr=args.lr, betas=(args.beta1, args.beta2))
+
+    for epoch in range(args.num_epochs):
+        total_d_loss = total_g_loss = 0
+        for images, _ in data_loader:
+            m = images.size(0)
+            images: Tensor = images.to(device)
+            # Create real and fake labels.
+            real_labels = torch.ones(m, 1).to(device)
+            fake_labels = torch.zeros(m, 1).to(device)
+            # ================================================================== #
+            #                      Train the discriminator                       #
+            # ================================================================== #
+
+            # Forward pass
+            outputs = D(images)
+            d_loss_real: Tensor = criterion(outputs, real_labels)
+            
+            z = torch.rand(m, args.latent_dim).to(device)
+            fake_images: Tensor = G(z)
+            outputs = D(fake_images)
+            d_loss_fake: Tensor = criterion(outputs, fake_labels)
+
+            # Backward pass
+            d_loss: Tensor = d_loss_real + d_loss_fake
+            optimizer_D.zero_grad()
+            d_loss.backward()
+            optimizer_D.step()
+            total_d_loss += d_loss
+
+            # ================================================================== #
+            #                        Train the generator                         #
+            # ================================================================== #
+            
+            # Forward pass
+            z = torch.rand(m, args.latent_dim).to(device)
+            fake_images: Tensor = G(z)
+            outputs = D(fake_images)
+            
+            # Backward pass
+            g_loss: Tensor = criterion(outputs, real_labels)
+            optimizer_G.zero_grad()
+            g_loss.backward()
+            optimizer_G.step()
+            total_g_loss += g_loss
+        print(f'''
+=====================================
+Epoch: [{epoch + 1}/{args.num_epochs}]
+Discriminator Loss: {total_d_loss / len(data_loader):.4f}
+Generator Loss: {total_g_loss / len(data_loader):.4f}
+=====================================''')
+
+        # Log Discriminator and Generator loss.
+        writer.add_scalar('Discriminator Loss', total_d_loss / len(data_loader), epoch + 1)
+        writer.add_scalar('Generator Loss', total_g_loss / len(data_loader), epoch + 1)
+        fake_images: Tensor = G(fixed_noise)
+        img_grid = make_grid(denormalize(fake_images), nrow=8, padding=2)
+        writer.add_image('Fake Images', img_grid, epoch + 1)
+        if (epoch + 1) % args.interval == 0:
+            save_image(img_grid, os.path.join(args.sample_dir, args.dataset, f'fake_images_{epoch + 1}.png'))
+    # Save the model checkpoints.
+    torch.save(G.state_dict(), os.path.join(args.ckpt_dir, args.dataset, 'G.ckpt'))
+    torch.save(D.state_dict(), os.path.join(args.ckpt_dir, args.dataset, 'D.ckpt'))
+
+
+def main() -> None:
+    args = get_args()
+    setup(args)
+    C = 1 if args.dataset in ('MNIST', 'FashionMNIST') else 3
+    image_size = 32 if args.dataset in ('MNIST', 'FashionMNIST', 'CIFAR10') else 64
+    data_loader = get_data_loader(args)
+    # Generator and Discrminator.
+    G = Generator(num_channels=C, image_size=image_size, latent_dim=args.latent_dim).to(device)
+    D = Discriminator(num_channels=C, image_size=image_size).to(device)
+    G.apply(init_weight)
+    D.apply(init_weight)
+    train(args, G, D, data_loader)
+
+
+if __name__ == '__main__':
+    main()
